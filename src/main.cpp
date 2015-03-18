e6a481cf00c53b2f5bab6ef699a5f9dc898a04d3
David White <davewx7@gmail.com>
2015-03-17 21:49:35 -0700

    fix some bad --auto-update-module behavior on Linux

--- src/main.cpp
+++ src/main.cpp
@@ -111,8 +111,8 @@
 variant g_auto_update_info;
 
 namespace {
-	PREF_BOOL(force_auto_update, false, "Will do a forced sync of auto-updates");
 	PREF_BOOL(auto_update_module, false, "Auto updates the module from the module server on startup (number of milliseconds to spend attempting to update the module)");
+	PREF_BOOL(force_auto_update, false, "Will do a forced sync of auto-updates");
 	PREF_STRING(auto_update_anura, "", "Auto update Anura's binaries from the module server using the given name as the module ID (e.g. anura-windows might be the id for the windows binary)");
 	PREF_INT(auto_update_timeout, 5000, "Timeout to use on auto updates (given in milliseconds)");
 
@@ -498,11 +498,6 @@ extern "C" int main(int argcount, char* argvec[])
 		std::cerr << "LOADING CONFIGURATION FROM master-config.cfg" << std::endl;
 		variant cfg = json::parse_from_file("./master-config.cfg");
 		if(cfg.is_map()) {
-			if( cfg["id"].is_null() == false) {
-				std::cerr << "SETTING MODULE PATH FROM master-config.cfg: " << cfg["id"].as_string() << std::endl;
-				preferences::set_preferences_path_from_module(cfg["id"].as_string());
-				//XXX module::set_module_name(cfg["id"].as_string(), cfg["id"].as_string());
-			}
 			if(cfg["arguments"].is_null() == false) {
 				std::vector<std::string> additional_args = cfg["arguments"].as_list_string();
 				argv.insert(argv.begin(), additional_args.begin(), additional_args.end());
@@ -528,6 +523,7 @@ extern "C" int main(int argcount, char* argvec[])
 			arg_value = std::string(equal+1, arg.end());
 		}
 		if(arg_name == "--module") {
+			preferences::set_preferences_path_from_module(arg_value);
 			if(load_module(arg_value, &argv) != 0) {
 				bool auto_update = false;
 				for(size_t n = 0; n < argv.size(); ++n) {
