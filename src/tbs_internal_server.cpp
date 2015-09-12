3fc1bd1cf4242baf0a3901dfb0166f8e71175118
David White <davewx7@gmail.com>
2015-09-12 15:57:29 -0700

    fixed bug on Windows with starting utility processes with paths with spaces in them

--- src/tbs_internal_server.cpp
+++ src/tbs_internal_server.cpp
@@ -120,7 +120,7 @@ bool create_utility_process(const std::string& app, const std::vector<std::strin
 
 	// windows version
 	std::string command_line_params;
-	command_line_params += app_name_and_path + " ";
+	command_line_params += "\"" + app_name_and_path + "\" ";
 	for(size_t n = 0; n != argv.size(); ++n) {
 		command_line_params += argv[n] + " ";
 	}
