b271c1262635d264f7c904a9f55a1eb0d6365746
DDR <d@ddr0.ca>
2021-01-27 15:46:49 -0800

    Make imgui-based UIs baseline readable.
    	- Ah yes, this will be readable. Light blue on light grey. That'll
    	  wow 'em. So beautiful. (Sheesh. I've fixed this now.)
    	- Things are a lot darker now, since text *must* be light because a
    	  few key backgrounds are fixed-color, unthemable.

--- src/theme_imgui.cpp
+++ src/theme_imgui.cpp
@@ -43,34 +43,35 @@ namespace
 void theme_imgui_default()
 {
 	// Setup style
+	// TODO DDR 2021-01-27: Window bar colour and checkbox colour are not themable.
 	ImGuiStyle& style = ImGui::GetStyle();
-	style.Colors[ImGuiCol_Text] = ImVec4(0.31f, 0.25f, 0.24f, 1.00f);
-	style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
-	style.Colors[ImGuiCol_WindowBg] = ImVec4(0.94f, 0.94f, 0.94f, 1.00f);
+	style.Colors[ImGuiCol_Text] =                 ImVec4(255/255.0f, 255/255.0f, 255/255.0f, 255/255.0f);
+	style.Colors[ImGuiCol_TextDisabled] =         ImVec4(175/255.0f, 175/255.0f, 175/255.0f, 255/255.0f);
+	style.Colors[ImGuiCol_WindowBg] =             ImVec4( 29/255.0f,  28/255.0f,  34/255.0f, 255/255.0f);
 	style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.74f, 0.74f, 0.94f, 1.00f);
 	style.Colors[ImGuiCol_ChildWindowBg] = ImVec4(0.68f, 0.68f, 0.68f, 0.00f);
 	style.Colors[ImGuiCol_Border] = ImVec4(0.50f, 0.50f, 0.50f, 0.60f);
 	style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-	style.Colors[ImGuiCol_FrameBg] = ImVec4(0.62f, 0.70f, 0.72f, 0.56f);
+	style.Colors[ImGuiCol_FrameBg] =              ImVec4( 84/255.0f,  94/255.0f,  97/255.0f, 143/255.0f);
 	style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.95f, 0.33f, 0.14f, 0.47f);
 	style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.97f, 0.31f, 0.13f, 0.81f);
 	style.Colors[ImGuiCol_TitleBg] = ImVec4(0.42f, 0.75f, 1.00f, 0.53f);
 	style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.40f, 0.65f, 0.80f, 0.20f);
-	style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.40f, 0.62f, 0.80f, 0.15f);
-	style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.39f, 0.64f, 0.80f, 0.30f);
-	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.28f, 0.67f, 0.80f, 0.59f);
-	style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.25f, 0.48f, 0.53f, 0.67f);
-	style.Colors[ImGuiCol_ComboBg] = ImVec4(0.89f, 0.98f, 1.00f, 0.99f);
+	style.Colors[ImGuiCol_ScrollbarBg] =          ImVec4( 48/255.0f,  60/255.0f,  63/255.0f,  92/255.0f);
+	style.Colors[ImGuiCol_ScrollbarGrab] =        ImVec4( 83/255.0f, 112/255.0f, 171/255.0f, 255/255.0f);
+	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4( 71/255.0f, 171/255.0f, 204/255.0f, 150/255.0f);
+	style.Colors[ImGuiCol_ScrollbarGrabActive] =  ImVec4( 60/255.0f, 122/255.0f, 135/255.0f, 171/255.0f);
+	style.Colors[ImGuiCol_ComboBg] =              ImVec4( 89/255.0f,  98/255.0f, 100/255.0f, 255/255.0f);
 	style.Colors[ImGuiCol_CheckMark] = ImVec4(0.48f, 0.47f, 0.47f, 0.71f);
 	style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.31f, 0.47f, 0.99f, 1.00f);
-	style.Colors[ImGuiCol_Button] = ImVec4(1.00f, 0.79f, 0.18f, 0.78f);
-	style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.42f, 0.82f, 1.00f, 0.81f);
-	style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.72f, 1.00f, 1.00f, 0.86f);
-	style.Colors[ImGuiCol_Header] = ImVec4(0.65f, 0.78f, 0.84f, 0.80f);
-	style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.75f, 0.88f, 0.94f, 0.80f);
+	style.Colors[ImGuiCol_Button] =               ImVec4( 59/255.0f,  74/255.0f,  59/255.0f, 255/255.0f);
+	style.Colors[ImGuiCol_ButtonHovered] =        ImVec4( 87/255.0f,  97/255.0f,  90/255.0f, 255/255.0f);
+	style.Colors[ImGuiCol_ButtonActive] =         ImVec4( 60/ 29.0f,  95/255.0f,  86/255.0f, 255/255.0f);
+	style.Colors[ImGuiCol_Header] =               ImVec4( 59/255.0f,  71/255.0f,  76/255.0f, 204/255.0f);
+	style.Colors[ImGuiCol_HeaderHovered] =        ImVec4(140/255.0f, 173/255.0f, 189/255.0f, 204/255.0f);
 	style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.55f, 0.68f, 0.74f, 0.80f);
 	style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.60f, 0.60f, 0.80f, 0.30f);
-	style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(1.00f, 1.00f, 1.00f, 0.60f);
+	style.Colors[ImGuiCol_ResizeGripHovered] =    ImVec4(  0/255.0f,  75/255.0f, 178/255.0f, 255/255.0f);
 	style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(1.00f, 1.00f, 1.00f, 0.90f);
 	style.Colors[ImGuiCol_CloseButton] = ImVec4(0.41f, 0.75f, 0.98f, 0.50f);
 	style.Colors[ImGuiCol_CloseButtonHovered] = ImVec4(1.00f, 0.47f, 0.41f, 0.60f);
