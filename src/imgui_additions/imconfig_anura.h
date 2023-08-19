ad39b43fa86741b328168485d377411ead3ea6e2
Joni Orponen <joni.orponen@rokki.ch>
2023-08-19 10:16:47 +0200

    Integrate vcpkg into CMake.

--- src/imgui_additions/imconfig_anura.h
+++ src/imgui_additions/imconfig_anura.h
@@ -22,7 +22,3 @@
 #define IM_QUAT_CLASS_EXTRA		\
 	ImQuat(const glm::quat& f) { x = f.x; y = f.y; z = f.z; w = f.w; }	\
 	operator glm::quat() const { return glm::quat(w,x,y,z); }
-
-#ifndef IMGUI_DEFINE_MATH_OPERATORS
-#define IMGUI_DEFINE_MATH_OPERATORS     1
-#endif
